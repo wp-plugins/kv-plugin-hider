222e36b26e193b1e88a2df2e9d4efd18e8b739f4
kvvaradha <kvvaradha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-12 04:13:43 +0000

    KV Plugin Hider
    git-svn-id: https://plugins.svn.wordpress.org/kv-plugin-hider/trunk@912390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kv_plugin_hider.php
+++ kv_plugin_hider.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: KV Plugin Hider 
  * Plugin URI: http://kvcodes.com/
- * Description: generally all the plugins are apear  to your logged in users, if you want to hide a particular plugin to your users, just use this plugin to hide it from the plugins.php page.  But it will work if you activated the plugin.
+ * Description: Generally all the plug-ins are allowed to see your logged in users, If you want to hide a particular plugin From your users, Just use this plugin to hide it from the plugins.php page.  But it will work if you activated the plugin.
  * Author: Kvvaradha
  * Author URI: http://kvcodes.com/
  * Version: 1.0
